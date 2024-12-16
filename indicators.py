c02023ad660d1100572ac31f593ce934370cddfb
GandhiParth <parthg1910@gmail.com>
2024-12-16 12:15:32 +0530

    saving

--- indicators.py
+++ indicators.py
@@ -905,3 +905,584 @@ class Indicators:
         """
 
         raise NotImplementedError
+
+    def williams_ri(self, lookback: int = 14) -> "Indicators":
+        """
+        Calculates williams RI
+        """
+
+        self._lf = self._lf.with_columns(
+            (
+                (
+                    (pl.col("high").rolling_max(window_size=lookback) - pl.col("close"))
+                    / (
+                        pl.col("high").rolling_max(window_size=lookback)
+                        - pl.col("low").rolling_min(window_size=lookback)
+                    )
+                )
+                * -100
+            ).alias(f"williams_%r_{lookback}")
+        )
+        return self
+
+    def force_index(
+        self,
+    ) -> "Indicators":
+        """
+        Calculates Force Index
+        """
+
+        self._lf = self._lf.with_columns(
+            ((pl.col("close") - pl.col("close").shift(1)) * pl.col("volume")).alias(
+                "force_index"
+            )
+        )
+
+        return self
+
+    def negative_volume_index(self) -> "Indicators":
+        """
+        Calculates Negative Volume Index
+        """
+
+        self._lf = self._lf.with_columns(
+            (
+                pl.when(pl.col("volume") < pl.col("volume").shift(1))
+                .then((pl.col("close") / pl.col("close").shift(1) - 1).cum_sum())
+                .otherwise(0)
+            ).alias("nvi")
+        )
+
+        return self
+
+    def wma(self, columns: Union[List[str], pl.Expr], window_size: int) -> "Indicators":
+        """
+        Calculate Weighted Moving Average
+        """
+
+        raise NotImplementedError
+
+        columns = self._get_column_names(columns=columns)
+
+        weights = pl.Series("weights", range(1, window_size + 1))
+
+        self._lf = self._lf.with_columns(
+            (
+                pl.col(col).rolling_apply(
+                    function=lambda x: (x * weights[-len(x) :]).sum()
+                    / weights[-len(x) :].sum(),
+                    window_size=window_size,
+                )
+            ).alias(f"{col}_wma_{window_size}")
+            for col in columns
+        )
+
+    def donchian_channel(self, look_back: int = 20) -> "Indicators":
+        """
+        Calculate donchain channel
+        """
+
+        self._lf = self._lf.with_columns(
+            [
+                pl.col("high")
+                .rolling_max(window_size=look_back)
+                .alias(f"donchian_upper_{look_back}"),
+                pl.col("low")
+                .rolling_min(window_size=look_back)
+                .alias(f"donchian_lower_{look_back}"),
+                (
+                    (
+                        pl.col(f"donchian_upper_{look_back}")
+                        + pl.col(f"donchian_lower_{look_back}")
+                    )
+                    / 2
+                ).alias(f"donchian_mid_{look_back}"),
+            ]
+        )
+
+        return self
+
+    def aroon(self, look_back: int = 14) -> "Indicators":
+        """
+        Calculate Aroon
+        """
+
+        raise NotImplementedError
+
+        self._lf = self._lf.with_columns(
+            [
+                (
+                    100
+                    * (
+                        look_back
+                        - pl.col("high").rolling_apply(
+                            function=lambda x: len(x) - x.argmax(),
+                            window_size=look_back,
+                        )
+                    )
+                    / look_back
+                ).alias(f"aroon_up_{look_back}"),
+                (
+                    100
+                    * (
+                        look_back
+                        - pl.col("low").rolling_apply(
+                            function=lambda x: len(x) - x.argmin(),
+                            window_size=look_back,
+                        )
+                    )
+                    / look_back
+                ).alias(f"aroon_down_{look_back}"),
+            ]
+        )
+
+        return self
+
+    def chaikin_money_flow(self, look_back: int = 14) -> "Indicators":
+        """ """
+
+        self._lf = self._lf.with_columns(
+            (
+                (
+                    (
+                        pl.col("close")
+                        - pl.col("low")
+                        - (pl.col("high") - pl.col("close"))
+                    )
+                    / (pl.col("high") - pl.col("low"))
+                    * pl.col("volume")
+                ).rolling_sum(window_size=look_back)
+                / pl.col("volume").rolling_sum(window_size=look_back)
+            ).alias(f"cmf_{look_back}")
+        )
+
+        return self
+
+    def unlcer_index(
+        self, columns: Union[List[str], pl.Expr], look_back: int = 14
+    ) -> "Indicators":
+        """
+        Calculate Ulcer Index
+        """
+
+        columns = self._get_column_names(columns)
+
+        self._lf = self._lf.with_columns(
+            (
+                (
+                    (
+                        pl.col(col / pl.col(col).rolling_max(window_size=look_back) - 1)
+                        ** 2
+                    )
+                    .rolling_mean(window_size=look_back)
+                    .sqrt()
+                ).alias(f"{col}_ulcer_index_{look_back}")
+                for col in columns
+            )
+        )
+
+        return self
+
+    def dpo(
+        self, columns: Union[List[str], pl.Expr], look_back: int = 20
+    ) -> "Indicators":
+        """
+        Calculate Detrended Price Oscillator
+        """
+
+        columns = self._get_column_names(columns)
+
+        offset = (look_back // 2) + 1
+        self._lf = self._lf.with_columns(
+            (
+                pl.col(col)
+                - pl.col(col).rolling_mean(window_size=look_back).shift(offset)
+            ).alias(f"{col}_dpo_{look_back}")
+            for col in columns
+        )
+
+        return self
+
+    def kst_oscillator(self, columns: Union[List[str], pl.Expr]) -> "Indicators":
+        """ """
+
+        columns = self._get_column_names(columns=columns)
+
+        raise NotImplementedError
+
+        periods = [10, 15, 20, 30]  # Example periods
+        weights = [1, 2, 3, 4]  # Example weights
+        kst = sum(
+            weights[i]
+            * pl.col(column).rolling_apply(
+                function=lambda x: (x[-1] - x[0]) / x[0], window_size=periods[i]
+            )
+            for i in range(len(periods))
+        )
+        return lf.with_columns(kst.alias("kst_oscillator"))
+
+    def ease_of_movement(self):
+        """
+        Calculate Ease of Movement
+        """
+
+        self._lf = self._lf.with_columns(
+            (
+                (
+                    pl.col("high")
+                    + pl.col("low")
+                    - pl.col("high").shift(1)
+                    - pl.col("low").shift(1)
+                )
+                / pl.col("volume")
+            ).alias("ease_of_movement")
+        )
+
+        return self
+
+    def true_strength_index(
+        self, columns: Union[List[str], pl.Expr], short_period: int, long_preiod: int
+    ) -> "Indicators":
+        """
+        Calculates True Strength Index
+        """
+
+        columns = self._get_column_names(columns)
+
+        self._lf = (
+            self._lf.with_columns(
+                (pl.col(col) - pl.col(col).shift(1)).alias(f"_{col}_delta_price")
+                for col in columns
+            )
+            .with_columns(
+                pl.col(f"_{col}_delta_price")
+                .ewm_mean(span=short_period)
+                .ewm_mean(span=long_preiod)
+                .alias(f"_{col}_smooth_delta")
+                for col in columns
+            )
+            .with_columns(
+                pl.col(f"_{col}_delta_price")
+                .abs()
+                .ewm_mean(span=short_period)
+                .ewm_mean(span=long_preiod)
+                .alias(f"_{col}_smooth_abs_delta")
+                for col in columns
+            )
+            .with_columns(
+                (
+                    100
+                    * pl.col(f"_{col}_smooth_delta")
+                    / pl.col(f"_{col}_smooth_abs_delta")
+                ).alias(f"{col}_tsi_{short_period}_{long_preiod}")
+                for col in columns
+            )
+            .select(
+                pl.exclude(
+                    [f"_{col}_smooth{ab}_delta"]
+                    for col in columns
+                    for ab in ["_abs", ""]
+                )
+            )
+        )
+
+        return self
+
+    def ultimate_oscillator(
+        self,
+        short: int = 7,
+        medium: int = 14,
+        long: int = 28,
+        short_wt: int = 4,
+        medium_wt: int = 2,
+        long_wt: int = 2,
+    ) -> "Indicators":
+        """
+        Calculate Ultimate oscialltor
+        """
+
+        self._lf = (
+            self._lf.with_columns(
+                (
+                    pl.col("close")
+                    - pl.min_horizontal(pl.col("low"), pl.col("close").shift(1))
+                ).alias("_bp"),
+                (
+                    pl.max_horizontal(pl.col("high"), pl.col("close").shift(1))
+                    - pl.max_horizontal(pl.col("low"), pl.col("close").shift(1))
+                ).alias("_tr"),
+            )
+            .with_columns(
+                (
+                    (
+                        pl.col("_bp").rolling_mean(window_size=short)
+                        / pl.col("_tr").rolling_mean(window_size=short)
+                    )
+                    * short_wt
+                ).alias("_uo_short"),
+                (
+                    (
+                        pl.col("_bp").rolling_mean(window_size=medium)
+                        / pl.col("_tr").rolling_mean(window_size=medium)
+                    )
+                    * medium_wt
+                ).alias("_uo_medium"),
+                (
+                    (
+                        pl.col("_bp").rolling_mean(window_size=long)
+                        / pl.col("_tr").rolling_mean(window_size=long)
+                    )
+                    * long_wt
+                ).alias("_uo_long"),
+            )
+            .with_columns(
+                (
+                    (pl.sum_horizontal("_uo_short", "_uo_medium", "_uo_long") * 100)
+                    / (short_wt + medium_wt + long_wt)
+                ).alias(f"UO_{short}_{medium}_{long}_{short_wt}_{medium_wt}_{long_wt}")
+            )
+            .select(pl.exclude(["_bp", "_tr", "_uo_short", "_uo_medium", "_uo_long"]))
+        )
+
+        return self
+
+    def keltner_channel(
+        self, ema_period: int = 20, atr_period: int = 14, multiplyer: float = 2
+    ) -> "Indicators":
+        """
+        Calculate Keltner Channel
+        """
+
+        self._lf = (
+            self._lf.with_columns(
+                (
+                    pl.max_horizontal(pl.col("high"), pl.col("close").shift(1))
+                    - pl.min_horizontal(pl.col("low"), pl.col("close").shift(1))
+                ).alias("_tr"),
+                pl.col("close").ewm_mean(span=ema_period).alias("_middle_band"),
+            )
+            .with_columns(
+                pl.col("_tr").rolling_mean(window_size=atr_period).alias("_atr"),
+            )
+            .with_columns(
+                (pl.col("_middle_band") + pl.col("_atr") * multiplyer).alias(
+                    f"KC_Upper_Band_{ema_period}_{atr_period}_{multiplyer}"
+                ),
+                (pl.col("_middle_band") - pl.col("_atr") * multiplyer).alias(
+                    f"KC_Lower_Band_{ema_period}_{atr_period}_{multiplyer}"
+                ),
+            )
+            .select(pl.exclude(["_tr", "_atr", "_middle_band"]))
+        )
+
+        return self
+
+    def kst(
+        self,
+        columns: Union[List[str], pl.Expr],
+        roc_periods: tuple = (10, 15, 20, 30),
+        sma_periods: tuple = (10, 10, 10, 15),
+        weights: tuple = (1, 2, 3, 4),
+    ) -> "Indicators":
+        """
+        Calculate KST Indicators
+        """
+
+        raise NotImplementedError
+
+        columns = self._get_column_names(columns)
+        assert len(roc_periods) == len(sma_periods) == len(weights)
+
+        rocs = []
+        for i, (roc_p, sma_p, w) in enumerate(
+            zip(roc_periods, sma_periods, weights), start=1
+        ):
+            roc = (
+                (pl.col(column) - pl.col(column).shift(roc_p))
+                / pl.col(column).shift(roc_p)
+                * 100
+            ).alias(f"ROC_{i}")
+            smoothed_roc = (
+                pl.col(f"ROC_{i}")
+                .rolling_mean(window_size=sma_p)
+                .alias(f"Smoothed_ROC_{i}")
+            )
+            weighted_roc = (pl.col(f"Smoothed_ROC_{i}") * w).alias(f"Weighted_ROC_{i}")
+            rocs.extend([roc, smoothed_roc, weighted_roc])
+
+        return (
+            lf.with_columns(rocs)
+            .with_columns(
+                pl.sum(
+                    [
+                        pl.col(f"Weighted_ROC_{i}")
+                        for i in range(1, len(roc_periods) + 1)
+                    ]
+                ).alias("KST")
+            )
+            .drop(
+                [f"ROC_{i}" for i in range(1, len(roc_periods) + 1)]
+                + [f"Smoothed_ROC_{i}" for i in range(1, len(roc_periods) + 1)]
+                + [f"Weighted_ROC_{i}" for i in range(1, len(roc_periods) + 1)]
+            )
+        )
+
+    def stc(
+        self,
+        columns: Union[List[str], pl.Expr],
+        fast_period: int = 12,
+        slow_period: int = 26,
+        signal_period: int = 9,
+        stoch_period: int = 10,
+    ) -> "Indicators":
+        """
+        Calculates schaff cycle trend
+        """
+
+        columns = self._get_column_names(columns)
+        raise NotImplementedError
+
+        self._lf = (
+            self._lf.with_columns(
+                (
+                    pl.col(col).ewm_mean(span=fast_period)
+                    - pl.col(col).ewm_mean(span=slow_period)
+                ).alias(f"_{col}_macd_line")
+                for col in columns
+            )
+            .with_columns(
+                pl.col(f"_{col}_macd_line")
+                .ewm_mean(span=signal_period)
+                .alias(f"_{col}_signal_line")
+                for col in columns
+            )
+            .with_columns(
+                (
+                    (
+                        pl.col(f"_{col}_macd_line")
+                        - pl.col(f"_{col}_macd_line").rolling_min(
+                            window_size=stoch_period
+                        )
+                    )
+                    / (
+                        pl.col(f"_{col}_macd_line").rolling_max(
+                            window_size=stoch_period
+                        )
+                        - pl.col(f"_{col}_macd_line").rolling_min(
+                            window_size=stoch_period
+                        )
+                    )
+                    * 100
+                ).alias(f"{col}_stc_{fast_period}_{slow_period}_{stoch_period}")
+                for col in columns
+            )
+            .select(pl.exclude([]))
+        )
+
+    def kama(self):
+        raise NotImplementedError
+
+        def kama(lf: pl.LazyFrame, price_col: str, window: int = 10) -> pl.LazyFrame:
+            fast_sc = 2 / (2 + 1)
+            slow_sc = 2 / (30 + 1)
+
+            return (
+                lf.with_columns(
+                    (
+                        (
+                            pl.col(price_col)
+                            - pl.col(price_col).shift(window).fill_null(0)
+                        )
+                        / (
+                            pl.col(price_col)
+                            - pl.col(price_col)
+                            .shift(1)
+                            .abs()
+                            .rolling_sum(window_size=window)
+                        )
+                    ).alias("ER")
+                )
+                .with_columns(
+                    ((pl.col("ER") * (fast_sc - slow_sc) + slow_sc) ** 2).alias("SC")
+                )
+                .with_columns(
+                    pl.col(price_col)
+                    .ewm_mean(alpha=pl.col("SC"))
+                    .alias(f"KAMA_{price_col}")
+                )
+                .select(pl.all().exclude(["ER", "SC"]))
+            )
+
+    def adi(self):
+        raise NotImplementedError
+
+        def accumulation_distribution_index(
+            lf: pl.LazyFrame,
+            high_col: str,
+            low_col: str,
+            close_col: str,
+            volume_col: str,
+        ) -> pl.LazyFrame:
+            return (
+                lf.with_columns(
+                    (
+                        (
+                            (pl.col(close_col) - pl.col(low_col))
+                            - (pl.col(high_col) - pl.col(close_col))
+                        )
+                        / (pl.col(high_col) - pl.col(low_col)).fill_nan(0)
+                    ).alias("MFM")
+                )
+                .with_columns((pl.col("MFM") * pl.col(volume_col)).alias("MFV"))
+                .with_columns(pl.col("MFV").cumsum().alias("ADI"))
+                .select(pl.all().exclude(["MFM", "MFV"]))
+            )
+
+    def mfi(self):
+        raise NotImplementedError
+
+        def money_flow_index(
+            lf: pl.LazyFrame,
+            high_col: str,
+            low_col: str,
+            close_col: str,
+            volume_col: str,
+            window: int = 14,
+        ) -> pl.LazyFrame:
+            return (
+                lf.with_columns(
+                    # Typical Price
+                    (
+                        (pl.col(high_col) + pl.col(low_col) + pl.col(close_col)) / 3
+                    ).alias("TP")
+                )
+                .with_columns(
+                    # Raw Money Flow
+                    (pl.col("TP") * pl.col(volume_col)).alias("RMF")
+                )
+                .with_columns(
+                    # Positive/Negative Money Flow
+                    pl.when(pl.col("TP") > pl.col("TP").shift(1))
+                    .then(pl.col("RMF"))
+                    .otherwise(0)
+                    .rolling_sum(window_size=window)
+                    .alias("Positive_MF"),
+                    pl.when(pl.col("TP") <= pl.col("TP").shift(1))
+                    .then(pl.col("RMF"))
+                    .otherwise(0)
+                    .rolling_sum(window_size=window)
+                    .alias("Negative_MF"),
+                )
+                .with_columns(
+                    # Money Flow Index
+                    (
+                        100
+                        - (100 / (1 + (pl.col("Positive_MF") / pl.col("Negative_MF"))))
+                    ).alias("MFI")
+                )
+                .select(
+                    pl.all().exclude(
+                        ["TP", "RMF", "Positive_MF", "Negative_MF"]
+                    )  # Remove intermediate calculations
+                )
+            )
